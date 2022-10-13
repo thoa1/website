0675dccea878b66d5532bb4507b9488fa6afe761
Travis Hoa <thoa1@CS544-thoa1.bu.binghamton.edu>
2022-10-13 03:13:32 +0000

    update

--- submit/prj2-sol/src/contacts-dao.mjs
+++ submit/prj2-sol/src/contacts-dao.mjs
@@ -36,13 +36,33 @@ class ContactsDao {
     try {
     	p._client = await(new MongoClient(dbUrl)).connect();
     	const db = p.client.db();
-    	const users = db.collection(USERS_COLLECTION);
-    	const contacts = db.collection(CONTACTS_COLLECTION);
+    	const collections = await(db.listCollections().toArray());
+    	
+    	const user_validate = !!collections.find(c => c.name == "users");
+    	const contacts_validate = !!collections.find(c => c.name == "contacts");
+    	
+    	const choices = {collation: {locale: 'en', strength:2,}};
+    	if(!user_validate){
+    		const collection =await db.createCollection("users", choices);
+    	}
+    	
+    	if( contacts_validate){
+    		const collection = await db.Collection("contacts");
+    		await collection.createIndex({"emails":1});
+    		await collection.createIndex({"prefix":1}, choices);
+    		
+    	}
+    	else{
+    		const collection = await db.createCollection("contacts", choices);
+    		await collection.createIndex({"emails":1});
+    		await collection.createIndex({"prefix":1}, choices);
+    	}
+    	const users = db.collection("users");
+    	const contacts = db.collection("contacts");
     	p.users = users;
     	p.contacts = contacts;
     	
-    	this.#makeIn();
-    	
+ 
     	return okResult(new ContactsDao(p));
     	
     
@@ -85,12 +105,9 @@ class ContactsDao {
     //TODO any setup code
     try {
     	const c = this.users;
-    	const cs = await(users.find({}).toArray());
-    	for(const k of cs){
-    		this.clear(k._id);
-    	}
-      await this.contacts.deleteMany({});
-      return okResult(cs.length);
+    	const info = await c.deleteMany({});
+    
+      return okResult(info.deletedCount);
     }
     catch (error) {
       console.error(error);
@@ -106,8 +123,8 @@ class ContactsDao {
     //TODO any setup code
     try {
     	const c = this.contacts;
-    	const delete_info = await collection.deleteMany({userId: {userId}});
-    	return okResult(delete_info.result.n);
+    	const info = await c.deleteMany({"userId": {userId}});
+    	return okResult(info.deletedCount);
     	
     }
     catch (error) {
@@ -127,14 +144,16 @@ class ContactsDao {
    *    BAD_REQ: contact contains an _id property
    *    DB: a database error occurred   
    */
-  async #nextId(){
- 		const query = {_id: NEXT_ID_KEY};
- 		const update = {$inc: {[NEXT_ID_KEY]: 1}};
- 		const choices = { upsert: true, returnDocument: 'after'};
- 		const ret = await this.contacts.findOneAndUpdate(query, update, choices);
- 		const seq = ret.value[next_id];
- 		return String(seq) + Math.random().toFixed(10).replace(/^0\./, '_');
- 	}
+	const NEXT_ID_KEY = 'count';
+	
+	async #nextId(){
+		const query = {_id: NEXT_ID_KEY};
+		const update = {$inc: {[NEXT_ID_KEY]: 1}};
+		const choices = { upsert: true, returnDocument: 'after'};
+		const ret = await this.contacts.findOneAndUpdate(query, update, choices);
+		const seq = ret.value[next_id];
+		return String(seq) + Math.random().toFixed(10).replace(/^0\./, '_');
+	}
  	
   async create(contact) {
     //TODO any setup code
@@ -142,12 +161,14 @@ class ContactsDao {
     	if (contact._id){
     		return errResult("new contact can not have id", {code: "BAD_REQ"});
     	}
-    	const cID = await this.#nextId();
-    	const dbObject = {_id: cID, ...contact};
+    	const cID = new ObjectId().toHexString();
+    	const prefix = namePrefixes(contact.name);
+    	
+    	const dbObject = {"_id": cID, "prefix":prefix, ...contact};
     	const ArrPre = namePrefixes(contact.name);
-    	if(ArrPre){
+    	if(prefix){
     		const collection = this.contacts;
-    		await db.contacts.insertOne(dbObject);
+    		await this.contacts.insertOne(dbObject);
     		
     	}
       return okResult(cID);
@@ -170,8 +191,14 @@ class ContactsDao {
     	const collection = this.contacts;
     	const dbEn = await collection.find({"userId": userId, "contactId": id});
     	
-    	
-      return okResult({id, ...dbEn});
+    	if(dbEn){
+    		delete dbEn._id;
+    		return okResult(dbEn);
+			}
+    	else{
+    		return errResult("no contact for contactid", { code: 'NOT_FOUND' });
+			}	
+      
     }
     catch (error) {
       console.error(error);
@@ -196,31 +223,34 @@ class ContactsDao {
    */
   async search({userId, id, prefix, email, index=0, count=DEFAULT_COUNT}={}) {
     //TODO any setup code
+    let ret = [];
     try {
-    	
-      return errResult('TODO', { code: 'TODO' });
+    	if(!id &&  !prefix &&email){
+    		const cursor = await(this.contacts.find({"userID":userId}));
+    		ret = ret.concat(await cursor.sort({name:1}).skip(index).limit(count).toArray());
+			}
+			if(prefix){
+				const cursor = await(this.contacts.find({"userId":userId, "prefix":{$elemMatch:{$eq:prefix}}}));
+				ret = ret.concat(await cursor.sort({name:1 }).skip(index).limit(count).toArray());
+				
+			}
+			if(email){
+				const cursor = await(this.contacts.find({"userID":userId, "emails":email}));
+    		ret = ret.concat(await cursor.sort({name:1}).skip(index).limit(count).toArray());
+			}
+			if(id){
+				const cursor = await(this.contacts.find({"userID":userId, "_id":id}));
+    		ret = ret.concat(await cursor.sort({name:1}).skip(index).limit(count).toArray());
+			}
+      return okResult(ret);
     }
     catch (error) {
       console.error(error);
       return errResult(error.message, { code: 'DB' });
     }
   }
-  
-	async #makeIn(){
-  	const collection = await(db.listCollections().toArray());
-  	const exists = !!collections.find(c => c.name === property);
-  	
-  	if (exists){
-  		await db.collection(property).createIndex(property);
-  		
-  	}
-  	else{
-  		const choices = {collation: {locale: 'en', strength 2,}};
-  		const collection = await db.createCollection(property, options);
-  		collection.createIndex(property);
-  	}
- 
-  }
+
+
 
   
  	
